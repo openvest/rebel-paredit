b15e7f12f3c3740fb91d682c619d722167d2f219
Phil Cooper <philip.cooper@openvest.com>
2024-08-02 16:04:30 -0600

    private access

--- rebel-readline/repl/use_readline.clj
+++ rebel-readline/repl/use_readline.clj
@@ -36,3 +36,42 @@
 #_ ;; but this does not
 (binding [*in* (java.io.PushbackReader. (io/reader (.getBytes "hello there")))]
   (.readLine line-reader))
+
+
+(def ap (:autopair-widgets @line-reader))
+(def ap-can-skip-field
+  (doto
+    (.getDeclaredMethod (class ap) "canSkip" (into-array [java.lang.String]))
+    (.setAccessible true)))
+
+(comment
+  ;; TODO invoke not working
+  (.invoke ap-can-skip-field ap (into-array Object ["hi"]))
+  (def a (into-array ["hi"]))
+
+  (def lr (LineReaderImpl. j/*terminal*))
+  (def ap (proxy [AutopairWidgets] [lr true]
+            )))
+
+;; can't get this to work
+;; works for 0 arity methods only
+;; works: (invoke-private-method "SomeLongStringToSubsequence" "subSequence" (int 4) (int 14))
+;; works: (invoke-private-method "someThing" "equalsIgnoreCase" "Something")
+(defn invoke-private-method
+  [obj fn-name & args]
+  (let [m (->> (.. obj getClass getDeclaredMethods)
+               (filter (fn [x] (.. x getName (equals fn-name))))
+               (filter (fn [x] (= (count args) (.getParameterCount x))))
+               first)
+        m-args (into-array Object args)]
+    (.setAccessible m true)
+    (.invoke m obj m-args)))
+
+;; seems to work OK
+(defn get-private-field
+  [obj fn-name]
+  (let [f (->> (.. obj getClass getDeclaredFields)
+               (filter (fn [x] (.. x getName (equals fn-name))))
+               first)]
+    (.setAccessible f true)
+    (.get f obj)))
